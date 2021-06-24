3fb46129568ae5af1190749f25b05035adeee2f2
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-06-24 20:35:07 -0300

    prueba dos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>EL PEPE</p>
+        <p>El Albers xd</p>
         <a
           className="App-link"
           href="https://reactjs.org"
