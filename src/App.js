1eab26b8c7fbc70af28715d33db99a066ae03816
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-06-28 20:57:49 -0300

    fix en imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./Common/chakraConfig/theme";
-import { Routes } from "./Router/index";
+import { Routes } from "./Routers/index";
 
 function App() {
   return (
